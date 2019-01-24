80cea59c98ff592406794b1255ba5308e960e27f
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2019-01-24 18:22:20 +0530

    Create Bon_Appetit.java

---
+++ Bon_Appetit.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the bonAppetit function below.
+    static void bonAppetit(List<Integer> bill, int k, int b) {
+        int arr[]=new int[bill.size()];
+        for(int i=0;i<bill.size();i++)
+        {
+            arr[i]=bill.get(i);
+        }
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(i==k)
+            continue;
+            else
+            sum=sum+arr[i];
+        }
+        if(sum/2==b)
+        System.out.println("Bon Appetit");
+        else
+        System.out.println(b-(sum/2));
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] nk = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        String[] billItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> bill = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int billItem = Integer.parseInt(billItems[i]);
+            bill.add(billItem);
+        }
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bonAppetit(bill, k, b);
+
+        bufferedReader.close();
+    }
+}
